e9e0d72afa95983420a76a897ed0ad3a7e6b165d
renoeno <reno.less@gmail.com>
2022-07-03 13:09:37 -0300

    First ocmmit

---
+++ .storybook/main.js
@@ -0,0 +1,31 @@
+const webpackBase = require("../webpack.config");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: "@storybook/web-components",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: (config) => {
+    config.module.rules.push(...webpackBase.module.rules);
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: "node_modules/@renoeno/design-tokens-meiuca/dist/css/globals.css",
+            to: "tokens/globals.css",
+          },
+          {
+            from: `**/**/*.css`,
+            context: `node_modules/@renoeno/design-tokens-meiuca/dist/css`,
+            to: "tokens/[path]/[name][ext]",
+          },
+        ],
+      })
+    );
+
+    return config;
+  },
+};
